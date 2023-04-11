4bd55a26596adc5228f7233670a3213170e8de02
yes <107295762+NgKhyHern@users.noreply.github.com>
2023-04-11 23:09:58 +0800

    sdfdsobfAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Nexercise_Server.ino
+++ Nexercise_Server.ino
@@ -1,75 +1,81 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-#define ledPin 13 // Pin connected to the LED
-#define button1Pin 5 // Pin connected to button 1
-#define button2Pin 14 // Pin connected to button 2
-
+// Replace with your network credentials
 const char* ssid = "Whateverthisis"; // Your WiFi SSID
 const char* password = "VeryGoodPassword"; // Your WiFi password
 
-WiFiServer server(80); // Create a WiFi server on port 80
+// Create an instance of the server
+ESP8266WebServer server(80);
+
+// Variables to store the button status
+volatile bool button1Status = false;
+volatile bool button2Status = false;
+
+void ICACHE_RAM_ATTR handleButton1() {
+  Serial.println("Button 1 Pressed");
+  // Toggle the button 1 status
+  button1Status = !button1Status;
+
+  // Send the updated button 1 status to the client-side ESP8266
+  String status = button1Status ? "on" : "off";
+  server.send(200, "text/plain", status);
+}
+
+void ICACHE_RAM_ATTR handleButton2() {
+  Serial.println("Button 2 Pressed");
+  // Toggle the button 2 status
+  button2Status = !button2Status;
+
+  // Send the updated button 2 status to the client-side ESP8266
+  String status = button2Status ? "on" : "off";
+  server.send(200, "text/plain", status);
+}
+
+void handleRoot() {
+  String html = "<html><body>";
+  html += "<h1>Server-side ESP8266</h1>";
+  html += "<h2>Button 1 Status: ";
+  html += button1Status ? "ON" : "OFF";
+  html += "</h2>";
+  html += "<h2>Button 2 Status: ";
+  html += button2Status ? "ON" : "OFF";
+  html += "</h2>";
+  html += "<h3><a href=\"/button1\">Toggle Button 1</a></h3>";
+  html += "<h3><a href=\"/button2\">Toggle Button 2</a></h3>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
 
 void setup() {
+  // Start Serial communication
   Serial.begin(115200);
-  delay(10);
 
-  pinMode(ledPin, OUTPUT); // Set the LED pin as OUTPUT
-  pinMode(button1Pin, INPUT_PULLUP); // Set button 1 pin as INPUT_PULLUP
-  pinMode(button2Pin, INPUT_PULLUP); // Set button 2 pin as INPUT_PULLUP
-
-  // Connect to WiFi
-  WiFi.mode(WIFI_STA);
+  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("Connected to WiFi. IP:");
+  Serial.print(WiFi.localIP());
 
-  server.begin(); // Start the server
+  // Configure the button pins as input with internal pull-up resistors
+  pinMode(5, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+
+  // Attach interrupts to the button pins to detect button presses
+  attachInterrupt(digitalPinToInterrupt(5), handleButton1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(14), handleButton2, CHANGE);
+
+  // Start the server
+  server.on("/", handleRoot);
+  server.on("/button1", handleButton1);
+  server.on("/button2", handleButton2);
+  server.begin();
   Serial.println("Server started");
 }
 
 void loop() {
-  WiFiClient client = server.available(); // Check for client connection
-
-  if (client) {
-    Serial.println("New client connected");
-
-    while (client.connected()) {
-      if (client.available()) {
-        String request = client.readStringUntil('\r'); // Read client request
-        client.flush();
-
-        if (request.indexOf("/led/on") != -1) { // If request contains /led/on
-          digitalWrite(ledPin, HIGH); // Turn on the LED
-          client.println("LED turned on");
-        }
-        else if (request.indexOf("/led/off") != -1) { // If request contains /led/off
-          digitalWrite(ledPin, LOW); // Turn off the LED
-          client.println("LED turned off");
-        }
-        else {
-          client.println("Invalid request"); // Send error message for invalid requests
-        }
-
-        // Check button 1 status
-        if (digitalRead(button1Pin) == LOW) {
-          client.println("Button 1 pressed"); // Send message to client about button 1 status
-        }
-
-        // Check button 2 status
-        if (digitalRead(button2Pin) == LOW) {
-          client.println("Button 2 pressed"); // Send message to client about button 2 status
-        }
-
-        delay(10);
-        client.stop(); // Close the connection
-        Serial.println("Client disconnected");
-      }
-    }
-  }
+  server.handleClient();
 }
