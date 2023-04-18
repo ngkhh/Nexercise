89c0e1b632abc4b5e748b2948669759bdf3bdcae
stevejobless <seeruizhe0700@gmail.com>
2023-04-18 18:53:23 +0800

    Update lednbuttondemo.ino
    re-wrote shits

--- lednbuttondemo.ino
+++ lednbuttondemo.ino
@@ -1,92 +1,124 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Wi-Fi settings
-const char* ssid = "your_SSID";
-const char* password = "your_PASSWORD";
+// MQTT broker settings
+const char* mqttServer = "mqtt.example.com";
+const int mqttPort = 1883;
+const char* mqttUser = "your_mqtt_username";
+const char* mqttPassword = "your_mqtt_password";
+const char* ledTopic = "your_mqtt_led_topic";
 
-// MQTT settings
-const char* mqtt_server = "your_MQTT_server_address";
-const char* mqtt_username = "your_MQTT_username";
-const char* mqtt_password = "your_MQTT_password";
-const char* mqtt_topic = "your_MQTT_topic";
+// WiFi settings
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
 
 // Pin definitions
-#define BUTTON1_PIN 2
-#define BUTTON2_PIN 3
-#define LED_PIN 16
+const int buttonPin1 = 0; // Change to your first button's pin
+const int buttonPin2 = 2; // Change to your second button's pin
+const int ledPin = 16; // Change to your LED's pin
 
-// Create a Wi-Fi client object
 WiFiClient espClient;
+PubSubClient client(espClient);
 
-// Create a MQTT client object
-PubSubClient mqttClient(espClient);
+bool button1State = false;
+bool button2State = false;
+bool ledState = false;
 
 void setup() {
-  // Set up the button pins as inputs with pull-up resistors
-  pinMode(BUTTON1_PIN, INPUT_PULLUP);
-  pinMode(BUTTON2_PIN, INPUT_PULLUP);
-
-  // Set up the LED pin as an output
-  pinMode(LED_PIN, OUTPUT);
-
-  // Start serial communication
   Serial.begin(9600);
+  delay(10);
 
-  // Connect to Wi-Fi
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+
+  // Connect to WiFi
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
+    Serial.print(".");
   }
 
-  // Connect to MQTT server
-  mqttClient.setServer(mqtt_server, 1883);
-  while (!mqttClient.connected()) {
-    if (mqttClient.connect("ESP8266_Client", mqtt_username, mqtt_password)) {
-      mqttClient.subscribe(mqtt_topic);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Connect to MQTT broker
+  Serial.print("Connecting to MQTT broker ");
+  Serial.print(mqttServer);
+  Serial.print(":");
+  Serial.println(mqttPort);
+  client.setServer(mqttServer, mqttPort);
+
+  while (!client.connected()) {
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword)) {
+      Serial.println("Connected to MQTT broker");
+      client.subscribe(ledTopic);
     } else {
-      delay(1000);
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
     }
   }
 }
 
 void loop() {
-  // Check for button 1 press
-  if (digitalRead(BUTTON1_PIN) == LOW) {
-    // Send message to turn on LED
-    mqttClient.publish(mqtt_topic, "button1_on");
-    Serial.println("Button 1 is pressed - LED is on");
+  // Handle MQTT messages
+  if (client.connected()) {
+    client.loop();
+  } else {
+    Serial.println("Disconnected from MQTT broker, reconnecting...");
+    while (!client.connected()) {
+      if (client.connect("ESP8266Client", mqttUser, mqttPassword)) {
+        Serial.println("Connected to MQTT broker");
+        client.subscribe(ledTopic);
+      } else {
+        Serial.print("Failed to connect to MQTT broker, rc=");
+        Serial.print(client.state());
+        Serial.println(" retrying in 5 seconds");
+        delay(5000);
+      }
+    }
+  }
+  
+  // Handle button states
+  if (digitalRead(buttonPin1) == LOW) {
+    if (!button1State) {
+      button1State = true;
+      Serial.println("Button 1 pressed");
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+      client.publish(ledTopic, ledState ? "1" : "0");
+    }
+  } else {
+    button1State = false;
   }
 
-  // Check for button 2 press
-  if (digitalRead(BUTTON2_PIN) == LOW) {
-    // Send message to turn off LED
-    mqttClient.publish(mqtt_topic, "button2_on");
-    Serial.println("Button 2 is pressed - LED is off");
+  if (digitalRead(buttonPin2) == LOW) {
+    if (!button2State) {
+      button2State = true;
+      Serial.println("Button 2 pressed");
+      // Handle button 2 press here
+    }
+  } else {
+    button2State = false;
   }
 
-  // Check for messages from MQTT server
-  mqttClient.loop();
+  // Print status to serial monitor
+  Serial.print("Button 1: ");
+  Serial.print(button1State);
+  Serial.print(", Button 2: ");
+  Serial.print(button2State);
+  Serial.print(", LED: ");
+  Serial.println(ledState);
+
+  // Wait before checking button states again
+  delay(100);
 }
-
-// Callback function for handling MQTT messages
-void callback(char* topic, byte* payload, unsigned int length) {
-  String message = "";
-  for (int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
-
-  // Handle message to turn on LED
-  if (message == "button1_on") {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED is on");
-  }
-
-  // Handle message to turn off LED
-  if (message == "button2_on") {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED is off");
-  }
-}
-
-//ur mom gay
