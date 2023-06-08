611c8ba0578e49d71a537710b65e90ef631e6500
yes <107295762+ngkhh@users.noreply.github.com>
2023-06-08 22:11:45 +0800

    Update temp_dotmatrix_test.ino

--- temp_dotmatrix_test.ino
+++ temp_dotmatrix_test.ino
@@ -1,19 +1,26 @@
 #include "LedControl.h"
 
-LedControl lc=LedControl(12,11,10,1);
+
+// pin 12 (D6) is connected to the DataIn 
+// pin 15 (D7) is connected to the CS
+// pin 13 (D8) is connected to CLK
+#define B1 5 /*(D1) Button 1 */
+#define B2 4 /*(D2) Button 2 */
+
+LedControl lc=LedControl(12,15,13,1);
 
 unsigned long delaytime=1000;
 
 void setup() {
+  Serial.begin(9600);
   lc.shutdown(0,false);
   lc.setIntensity(0,8);
   lc.clearDisplay(0);
+  pinMode(B1,INPUT_PULLUP);
 }
 
-
-void loop() { 
+void displayW() {
   byte W[8]={0x82,0x82,0x82,0x92,0x92,0xAA,0xC6,0x82};
-  byte M[8]={0x82,0xC6,0xAA,0x92,0x92,0x82,0x82,0x82};
   lc.setRow(0,0,W[0]);
   lc.setRow(0,1,W[1]);
   lc.setRow(0,2,W[2]);
@@ -22,7 +29,9 @@ void loop() {
   lc.setRow(0,5,W[5]);
   lc.setRow(0,6,W[6]);
   lc.setRow(0,7,W[7]);
-  delay(delaytime);
+}
+void displayM() {
+  byte M[8]={0x82,0xC6,0xAA,0x92,0x92,0x82,0x82,0x82};
   lc.setRow(0,0,M[0]);
   lc.setRow(0,1,M[1]);
   lc.setRow(0,2,M[2]);
@@ -31,5 +40,9 @@ void loop() {
   lc.setRow(0,5,M[5]);
   lc.setRow(0,6,M[6]);
   lc.setRow(0,7,M[7]);
-  delay(delaytime);
+}
+void loop() {
+  int b1State = digitalRead(B1);
+  Serial.println(b1State);
+  delay(1000);
 }
