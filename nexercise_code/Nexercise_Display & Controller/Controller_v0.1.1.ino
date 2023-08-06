2fc3bfe26e181be3eb0c436a58db12c8634bfd05
stevejobless <seeruizhe0700@gmail.com>
2023-08-06 19:18:20 +0800

    Update Controller_v0.1.1.ino

--- nexercise_code/Nexercise_Display & Controller/Controller_v0.1.1.ino
+++ nexercise_code/Nexercise_Display & Controller/Controller_v0.1.1.ino
@@ -11,24 +11,29 @@ const char* password = "your_wifi_password";
 const char* firebaseHost = "nexercise-69420-default-rtdb.asia-southeast1.firebasedatabase.app";
 const char* firebaseAuth = "your_firebase_auth_token";
 
-// Pins for buttons
+// Pins for controller buttons and buzzer
 const int buttonWPIN = D1;
 const int buttonMPIN = D2;
+const int buzzerPin = D3;
 
-// Firebase variables
-int sessionNumber;
+// Game variables
+char userInput;
 unsigned long sessionStartTime;
-char pressedButton;
+unsigned long lastActivityTime;
+unsigned long userWakeTime;
+
+// Inactivity variables
+const unsigned long inactivityThreshold = 2400000; // 40 minutes (40 min * 60 sec * 1000 ms)
+const unsigned long sleepThreshold = 3000000;     // 50 minutes (50 min * 60 sec * 1000 ms)
+bool isSleepMode = false;
 
 void setupFirebase() {
+  // Initialize Firebase
   Firebase.begin(firebaseHost, firebaseAuth);
 }
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(buttonWPIN, INPUT_PULLUP);
-  pinMode(buttonMPIN, INPUT_PULLUP);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -41,15 +46,17 @@ void setup() {
   // Setup Firebase
   setupFirebase();
 
+  // Setup buzzer pin
+  pinMode(buzzerPin, OUTPUT);
+
   // Start a new session
-  sessionNumber = generateSessionNumber();
-  sessionStartTime = millis();
-  Serial.print("Starting session number: ");
-  Serial.println(sessionNumber);
+  startNewSession();
 }
 
-void loop() {
-  checkUserInput();
+void startNewSession() {
+  sessionStartTime = millis();
+  Serial.println("Starting a new session");
+  userInput = '-';
 }
 
 void checkUserInput() {
@@ -60,17 +67,49 @@ void checkUserInput() {
   }
 }
 
-void handleUserInput(char userInput) {
-  pressedButton = userInput;
-  recordUserButton(pressedButton, sessionNumber);
+void handleUserInput(char input) {
+  if (input == 'W' || input == 'M') {
+    userInput = input;
+    lastActivityTime = millis();
+  }
 }
 
-int generateSessionNumber() {
-  // Generate a random session number between 1 and 1000
-  return random(1, 1001);
+void recordSessionData() {
+  String path = "sessions/" + String(sessionStartTime);
+  Firebase.setString(fbdo, (path + "/start_time").c_str(), String(sessionStartTime).c_str());
+  Firebase.setString(fbdo, (path + "/end_time").c_str(), String(millis()).c_str());
+  Firebase.setString(fbdo, (path + "/user_input").c_str(), String(userInput).c_str());
 }
 
-void recordUserButton(char pressedButton, int sessionNumber) {
-  String path = "sessions/" + String(sessionNumber);
-  Firebase.setString(fbdo, (path + "/pressed_button").c_str(), String(pressedButton).c_str());
+void checkInactivity() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - lastActivityTime >= inactivityThreshold && !isSleepMode) {
+    Serial.println("Inactivity detected, starting a new round");
+    startNewSession();
+  }
+
+  if (currentMillis - lastActivityTime >= 2400000 && currentMillis - lastActivityTime < inactivityThreshold && !isSleepMode) {
+    // Play buzzer sound for alert after 40 minutes of inactivity
+    digitalWrite(buzzerPin, HIGH);
+    delay(1000);
+    digitalWrite(buzzerPin, LOW);
+  }
+
+  if (currentMillis - lastActivityTime >= sleepThreshold && !isSleepMode) {
+    Serial.println("Entering sleep mode");
+    isSleepMode = true;
+    // Add code here to put the device into sleep mode
+    // For example, you can turn off the controller components to conserve power
+    userWakeTime = millis();
+    recordSessionData();
+    // Add code here to wake up the device from sleep mode
+    Serial.println("Exiting sleep mode");
+    isSleepMode = false;
+  }
+}
+
+void loop() {
+  checkUserInput();
+  checkInactivity();
 }
